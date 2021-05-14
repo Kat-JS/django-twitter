d4000038628cb3bd0a1691fa863e3a7dc8e95008
unknown <jiefei.shi94@gmail.com>
2021-05-13 21:53:07 -0500

    add unit tests

--- twitter/urls.py
+++ twitter/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from accounts.api import views
+from friendships.api.views import FriendshipViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
 router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
+router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
